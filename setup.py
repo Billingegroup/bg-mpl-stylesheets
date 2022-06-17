c86e020f12638c19f7451f7094926e04ec048c0a
Simon Billinge <sb2896@columbia.edu>
2022-06-17 14:12:34 +0200

    bumped version to 0.2.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bg_mpl_stylesheet",  # Replace with your own name
-    version='test',
+    version='0.2.7',
     author="Simon J. L. Billinge",
     author_email="sb2896@columbia.edu",
     description="A package for using Billinge group style files",
