bc55f65e66678a4f0806c9830e9f0f1081d9a386
Caden Myers <cjm2304@columbia.edu>
2024-07-08 16:20:39 -0400

    black edits on everything

--- example/color_cycle.py
+++ example/color_cycle.py
@@ -8,22 +8,22 @@ plt.style.use(bg_mpl_style)
 
 
 x = np.arange(0, 1, 0.01)
-y = np.sin(3*x)
+y = np.sin(3 * x)
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 offset = -0.5  # the offset to plot difference curve
 
-cycle = plt.rcParams['axes.prop_cycle'].by_key()['color']
+cycle = plt.rcParams["axes.prop_cycle"].by_key()["color"]
 
 # plot the color cycles and corresponding color codes.
-for (i, c) in enumerate(cycle):
-    ax.plot(x, y+offset*i, label='\\{}'.format(c), color=c, linestyle='-')
+for i, c in enumerate(cycle):
+    ax.plot(x, y + offset * i, label="\\{}".format(c), color=c, linestyle="-")
 
 
 ax.set_xlim(0, 2.0)  # set x-axis lower and upper limits
 
-ax.legend(loc='upper right', frameon=False, prop={'size': 14}, ncol=1)
+ax.legend(loc="upper right", frameon=False, prop={"size": 14}, ncol=1)
 
 
 plt.show()
--- example/plot.py
+++ example/plot.py
@@ -7,16 +7,16 @@ from bg_mpl_stylesheet.bg_mpl_stylesheet import bg_mpl_style
 plt.style.use(bg_mpl_style)
 
 # load PDF data
-r, gcalc, dr, dg, gdiff = loadData('./CdSe.fgr').T
+r, gcalc, dr, dg, gdiff = loadData("./CdSe.fgr").T
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 offset = -0.5  # the offset to plot difference curve
 
-ax.plot(r, gcalc+gdiff)  # Experimental
+ax.plot(r, gcalc + gdiff)  # Experimental
 ax.plot(r, gcalc)  # Calculated
 ax.plot(r, gdiff + offset)  # Difference
-ax.axhline(y=offset, color='k', linestyle='--')
+ax.axhline(y=offset, color="k", linestyle="--")
 
 ax.set_xlim(2.0, 20.0)  # set x-axis lower and upper limits
 ax.set_xlabel("r ($\mathrm{\AA}$)")
