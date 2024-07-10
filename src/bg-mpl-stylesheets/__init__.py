dd9d9c9ca80b3935ca491bc17f15179b45f5feae
Caden Myers <cjm2304@columbia.edu>
2024-07-10 12:22:35 -0400

    src/

---
+++ src/bg-mpl-stylesheets/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+##############################################################################
+#
+# (c) 2024 The Trustees of Columbia University in the City of New York.
+# All rights reserved.
+#
+# File coded by: Billinge Group members and community contributors.
+#
+# See GitHub contributions for a more detailed list of contributors.
+# https://github.com/bg-mpl-stylesheets/bg-mpl-stylesheets/graphs/contributors
+#
+# See LICENSE.rst for license information.
+#
+##############################################################################
+
+"""A package for using Billinge group style files"""
+
+from pkgutil import extend_path
+
+__path__ = extend_path(__path__, __name__)
+
+# End of file
