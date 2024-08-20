859a763ac798a07c1ac2f5bf58fa58694a09dc54
Sangjoon Bob Lee <bobleesj@gmail.com>
2024-08-20 09:12:16 -0400

    Capitalize hex for consistency

--- src/bg_mpl_stylesheets/colors.py
+++ src/bg_mpl_stylesheets/colors.py
@@ -4,20 +4,20 @@ from enum import Enum
 class Colors(str, Enum):
     BG_BLUE = "#0B3C5D"
     BG_RED = "#B82601"
-    BG_GREEN = "#1c6b0a"
+    BG_GREEN = "#1C6B0A"
     BG_LIGHT_BLUE = "#328CC1"
-    BG_LIGHT_GREY = "#a8b6c1"
+    BG_LIGHT_GREY = "#A8B6C1"
     BG_YELLOW = "#D9B310"
     BG_BROWN = "#6C5050"
     BG_BURGUNDY = "#76323F"
     BG_OLIVE_GREEN = "#626E60"
     BG_MUTED_OLIVE = "#918770"
     BG_BEIGE = "#C09F80"
-    BG_GREY = "#b0b0b0ff"
+    BG_GREY = "#B0B0B0FF"
     # Add more colors as needed
 
     @classmethod
-    def get_color_name_from_hex(cls, hex_value: str) -> str:
+    def get_color_name(cls, hex_value: str) -> str:
         for color in cls:
             if color.value.lower() == hex_value.lower():
                 return color.name
