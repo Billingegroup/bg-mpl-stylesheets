e911903cf03ca0e1c2b8f9ff9d3ceebe52d56811
Sangjoon Bob Lee <bobleesj@gmail.com>
2024-08-20 16:48:42 -0400

    Update color names like BG_BLUE to bg_blue

--- src/bg_mpl_stylesheets/colors.py
+++ src/bg_mpl_stylesheets/colors.py
@@ -2,18 +2,18 @@ from enum import Enum
 
 
 class Colors(str, Enum):
-    BG_BLUE = "#0B3C5D"
-    BG_RED = "#B82601"
-    BG_GREEN = "#1C6B0A"
-    BG_LIGHT_BLUE = "#328CC1"
-    BG_LIGHT_GREY = "#A8B6C1"
-    BG_YELLOW = "#D9B310"
-    BG_BROWN = "#6C5050"
-    BG_BURGUNDY = "#76323F"
-    BG_OLIVE_GREEN = "#626E60"
-    BG_MUTED_OLIVE = "#918770"
-    BG_BEIGE = "#C09F80"
-    BG_GREY = "#B0B0B0FF"
+    bg_blue = "#0B3C5D"
+    bg_red = "#B82601"
+    bg_green = "#1C6B0A"
+    bg_light_blue = "#328CC1"
+    bg_light_grey = "#A8B6C1"
+    bg_yellow = "#D9B310"
+    bg_brown = "#6C5050"
+    bg_burgundy = "#76323F"
+    bg_olive_green = "#626E60"
+    bg_muted_olive = "#918770"
+    bg_beige = "#C09F80"
+    bg_grey = "#B0B0B0FF"
     # Add more colors as needed
 
     @classmethod
@@ -25,4 +25,4 @@ class Colors(str, Enum):
 
     @classmethod
     def get_bg_colors(cls) -> list[str]:
-        return [color.value for color in cls if color.name.startswith("BG")]
+        return [color.value for color in cls if color.name.startswith("bg")]
